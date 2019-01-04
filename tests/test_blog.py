e075bb39ddc08f83442be06abcc56f69a40d1b65
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2019-01-04 15:12:59 -0200

    Tests: validate Title in post creation and edition

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -70,3 +70,13 @@ def test_update(client, auth, app):
         db = get_db()
         post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
         assert post['title'] == 'updated'
+
+
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+))
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post(path, data={'title': '', 'body': ''})
+    assert b'Title is required.' in response.data
