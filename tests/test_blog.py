a19e60def52bd3b1de0856e6c1f61b7003bb508f
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2019-01-04 15:07:11 -0200

    Tests: successful blog update

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -59,3 +59,14 @@ def test_create(client, auth, app):
         db = get_db()
         count = db.execute('SELECT COUNT(id) FROM post').fetchone()[0]
         assert count == 2
+
+
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get('/1/update').status_code == 200
+    client.post('/1/update', data={'title': 'updated', 'body': ''})
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post['title'] == 'updated'
